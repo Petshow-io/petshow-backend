2ff4269cb5e234a8b0041712859832903856d31c
Eduardo <eduarddoj1@gmail.com>
2021-09-16 22:19:58 -0300

    develop-v1
    	:rocket: Trocando Quarkus p Spring

---
+++ src/test/java/com/io/petshow/PetshowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.petshow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
