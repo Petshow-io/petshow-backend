1d7442ce2ef522f7daf9bd74c4a2004d60e1e75a
Eduardo <eduarddoj1@gmail.com>
2021-09-16 22:38:43 -0300

    Pequenas refatorações

--- src/test/java/com/io/petshow/PetshowApplicationTests.java
+++ src/test/java/com/io/petshow/PetshowApplicationTests.java
@@ -3,10 +3,10 @@ package com.io.petshow;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class PetshowApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
